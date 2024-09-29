493c60ded6b16c6c1e86a53d696b73fb7662b5cc
gullomagico <33353590+gullomagico@users.noreply.github.com>
2024-09-29 17:23:18 +0200

    Add fancy logger

--- main.go
+++ main.go
@@ -5,28 +5,61 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/charmbracelet/log"
 )
+
+type LogLevelType struct {
+    level log.Level
+}
+
+// Implement the Set method for LogLevelType
+func (l *LogLevelType) Set(value string) error {
+    v, err := strconv.Atoi(value)
+    if err != nil {
+        return err
+    }
+    l.level = log.Level(v)
+    return nil
+}
+
+// Implement the String method for LogLevelType
+func (l *LogLevelType) String() string {
+    return strconv.Itoa(int(l.level))
+}
 
 var (
 	Token string
+	LogLevel LogLevelType
 )
 
 func init() {
-
 	flag.StringVar(&Token, "t", "", "Bot Token")
+	flag.Var(&LogLevel, "l", "Log level")
 	flag.Parse()
 }
 
 func main() {
 
-	dg, err := discordgo.New("Bot " + Token)
-	if err != nil {
-		fmt.Println("error creating Discord session,", err)
+	if Token == "" {
+		log.Fatal("No token provided. Please run with param \"-t <bot token>\"")
 		return
 	}
+
+	log.SetLevel(LogLevel.level)
+
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		log.Fatal("error creating Discord session,", "err", err)
+		return
+	}
+
+	dg.AddHandler(func(s *discordgo.Session, event *discordgo.Ready) {
+		log.Info(fmt.Sprintf("%s bot is ready", s.State.User))
+	})
 
 	// Register the messageCreate func as a callback for MessageCreate events.
 	dg.AddHandler(messageCreate)
@@ -36,12 +69,12 @@ func main() {
 	// Open a websocket connection to Discord and begin listening.
 	err = dg.Open()
 	if err != nil {
-		fmt.Println("error opening connection,", err)
+		log.Error("error opening connection,", err)
 		return
 	}
 
 	// Wait here until CTRL-C or other term signal is received.
-	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	log.Info("Bot is now running.  Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
