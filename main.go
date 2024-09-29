bba0c07a41d1a0609c166df765fbb736ef367119
gullomagico <33353590+gullomagico@users.noreply.github.com>
2024-09-30 00:23:03 +0200

    Refactor main.go to handle voice state updates in Discord

--- main.go
+++ main.go
@@ -21,6 +21,11 @@ func init() {
 	flag.StringVar(&LogLevel, "log", "info", "Log Level of logger. Valid levels are: debug, info, warn, error, fatal")
 	flag.Parse()
 
+	if Token == "" {
+		log.Fatal("No token provided. Please run with param \"-t <bot token>\"")
+		return
+	}
+
 	validLogLevels := map[string]bool{
 		"debug": true,
 		"info":  true,
@@ -32,14 +37,6 @@ func init() {
 	if !validLogLevels[LogLevel] {
 		log.Fatal("Invalid log level provided. Valid levels are: debug, info, warn, error, fatal")
 	}
-}
-
-func main() {
-
-	if Token == "" {
-		log.Fatal("No token provided. Please run with param \"-t <bot token>\"")
-		return
-	}
 
 	level, err := log.ParseLevel(LogLevel)
 	if err != nil {
@@ -47,6 +44,9 @@ func main() {
 		return
 	}
 	log.SetLevel(level)
+}
+
+func main() {
 
 	dg, err := discordgo.New("Bot " + Token)
 	if err != nil {
@@ -58,10 +58,11 @@ func main() {
 		log.Info(fmt.Sprintf("%s bot is ready", s.State.User))
 	})
 
-	// Register the messageCreate func as a callback for MessageCreate events.
 	dg.AddHandler(messageCreate)
+	dg.AddHandler(voiceStateUpdate)
 
-	dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+	dg.Identify.Intents = discordgo.IntentsGuildMessages | discordgo.IntentsGuilds | discordgo.IntentsGuildVoiceStates
 
 	// Open a websocket connection to Discord and begin listening.
 	err = dg.Open()
@@ -93,4 +94,61 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Content == "pong" {
 		s.ChannelMessageSend(m.ChannelID, "Ping!")
 	}
+}
+
+func voiceStateUpdate(s *discordgo.Session, m *discordgo.VoiceStateUpdate) {
+    user, err := s.User(m.UserID)
+    if err != nil {
+        log.Error("error getting user,", "err", err)
+        return
+    }
+
+    switch {
+    // Caso in cui l'utente si unisce per la prima volta a un canale vocale
+    case m.BeforeUpdate == nil && m.ChannelID != "":
+        log.Debug(fmt.Sprintf("%s has joined the channel %s", user.Username, m.ChannelID))
+        handleUserJoinedChannel(s, m.ChannelID, user)
+    
+    // Caso in cui l'utente lascia il canale (m.ChannelID è "")
+    case m.ChannelID == "" && m.BeforeUpdate != nil:
+        log.Debug(fmt.Sprintf("%s has left the channel %s", user.Username, m.BeforeUpdate.ChannelID))
+        handleUserLeftChannel(s, m.BeforeUpdate.ChannelID, user)
+    
+    // Caso in cui l'utente cambia canale vocale
+    case m.BeforeUpdate != nil && m.ChannelID != m.BeforeUpdate.ChannelID:
+        log.Debug(fmt.Sprintf("%s changed channel from %s to channel %s", user.Username, m.BeforeUpdate.ChannelID, m.ChannelID))
+        
+        // Prima gestiamo l'uscita dal vecchio canale
+        handleUserLeftChannel(s, m.BeforeUpdate.ChannelID, user)
+        
+        // Poi gestiamo l'entrata nel nuovo canale
+        handleUserJoinedChannel(s, m.ChannelID, user)
+
+    // Caso in cui l'utente non cambia canale, ma cambia stato mute/deaf
+    case m.ChannelID == m.BeforeUpdate.ChannelID:
+        if m.SelfMute != m.BeforeUpdate.SelfMute {
+            if m.SelfMute {
+                log.Debug(fmt.Sprintf("%s turned mute on", user.Username))
+            } else {
+                log.Debug(fmt.Sprintf("%s turned mute off", user.Username))
+            }
+        }
+        if m.SelfDeaf != m.BeforeUpdate.SelfDeaf {
+            if m.SelfDeaf {
+                log.Debug(fmt.Sprintf("%s turned deaf on", user.Username))
+            } else {
+                log.Debug(fmt.Sprintf("%s turned deaf off", user.Username))
+            }
+        }
+    }
+}
+
+func handleUserJoinedChannel(s *discordgo.Session, channelID string, user *discordgo.User) {
+    // log.Info(fmt.Sprintf("Handle user %s joining channel %s", user.Username, channelID))
+    // Aggiungi qui la logica
+}
+
+func handleUserLeftChannel(s *discordgo.Session, channelID string, user *discordgo.User) {
+    // log.Info(fmt.Sprintf("Handle user %s leaving channel %s", user.Username, channelID))
+    // Aggiungi qui la logica
 }
